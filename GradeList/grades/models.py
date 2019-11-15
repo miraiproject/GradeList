0ebf00f3b90e8df70c46f5f5f820090046144beb
maeclamar <kazuki.maeda.miraidenshi@gmail.com>
2019-11-15 16:28:16 +0900

    Nothing changed

--- GradeList/grades/models.py
+++ GradeList/grades/models.py
@@ -7,9 +7,6 @@ from statistics import mean
         
 class Grades(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-<<<<<<< HEAD
-=======
-    subject = models.CharField(max_length=50, null=False)
 >>>>>>> 993c3b1bb0d00c2d268c10b020ce0642aabb18c3
     english = models.PositiveIntegerField()
     math = models.PositiveIntegerField()
