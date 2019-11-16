664b664692a4d28e6f41554c8a80a3daa8f962ad
hanatsuyo <hanatsuyo.t5.14@gmail.com>
2019-11-16 11:49:05 +0900

    profile function

--- GradeList/grades/forms.py
+++ GradeList/grades/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Grades, Objections
+from .models import Grades, Objections, Profile
 
 class GradeForm(forms.ModelForm):
     class Meta:
@@ -10,3 +10,10 @@ class ObjectionForm(forms.ModelForm):
     class Meta:
         model = Objections
         fields = ('text',)
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields =['name','student_number','major','grade']
+        labels={'name':'名前', 'student_number':'学籍番号', 'major':'学部・専攻', 'grade':'学年'}
--- GradeList/grades/models.py
+++ GradeList/grades/models.py
@@ -20,3 +20,11 @@ class Objections(models.Model):
     text = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=50)
+    student_number = models.IntegerField()
+    major = models.CharField(max_length=50)
+    grade = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
